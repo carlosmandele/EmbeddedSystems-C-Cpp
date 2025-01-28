e5e772be84b549cf88365f5ba54a023ec1b27d88
carlosmandele <carlos.mandelle@gmail.com>
2025-01-27 23:43:48 -0300

    Add e configuração das variáveis globais voláteis

--- TP01controll_LED_U4/TP01controll_LED_U4.c
+++ TP01controll_LED_U4/TP01controll_LED_U4.c
@@ -5,3 +5,8 @@
 // Configuração dos pinos para o LED e botão
 const uint LED_PIN = 13;   
 const uint BTN_A_PIN = 5;
+
+// Variáveis globais voláteis para sincronização com interrupções
+volatile int button_press_count = 0; // Contador de pressões do botão
+volatile int blink_count = 0;        // Contador de pisca-piscas do LED
+volatile bool led_state = false;     // Estado do LED
