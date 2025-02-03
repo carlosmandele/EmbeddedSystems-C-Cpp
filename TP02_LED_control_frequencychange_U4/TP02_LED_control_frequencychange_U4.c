981b5516636edc9f708788bc9c24c9a83c7860f1
carlosmandele <carlos.mandelle@gmail.com>
2025-02-03 20:09:28 -0300

    Inclusão de bibliotecas, configuração de pinos e botões

--- TP02_LED_control_frequencychange_U4/TP02_LED_control_frequencychange_U4.c
+++ TP02_LED_control_frequencychange_U4/TP02_LED_control_frequencychange_U4.c
@@ -1,14 +1,8 @@
-#include <stdio.h>
 #include "pico/stdlib.h"
+#include "hardware/timer.h"
+#include <stdio.h>
 
-
-
-int main()
-{
-    stdio_init_all();
-
-    while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
-    }
-}
+// Configuração dos pinos para o LED e botões
+const uint LED_PIN = 13;   
+const uint BTN_A_PIN = 5;
+const uint BTN_B_PIN = 6;
