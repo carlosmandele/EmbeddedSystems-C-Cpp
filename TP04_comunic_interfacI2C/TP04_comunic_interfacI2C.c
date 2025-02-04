232343f180dfc4d9c2e8b1ebee600215a2129859
carlosmandele <carlos.mandelle@gmail.com>
2025-02-03 23:47:53 -0300

    Add bibliotecas, definição de Constantes e Variáveis globais

--- TP04_comunic_interfacI2C/TP04_comunic_interfacI2C.c
+++ TP04_comunic_interfacI2C/TP04_comunic_interfacI2C.c
@@ -1,1 +1,15 @@
+#include "pico/stdlib.h"
+#include "hardware/i2c.h"
+#include "hardware/irq.h"
+#include <stdio.h>
+
+// Definições de Constantes
+#define I2C_PORT i2c0
+#define DS1307_I2C_ADDR 0x68
+#define BUFFER_SIZE 7
+
+// Variáveis Globais
+volatile bool data_received = false;
+uint8_t data_buffer[BUFFER_SIZE];
+
 
