54087db0657fcb1c256ce5efc5471df710c826ef
carlosmandele <carlos.mandelle@gmail.com>
2025-02-03 22:45:07 -0300

    Inclusão de bibliotecas e definição de pinos

--- TP03_UARTinterface_data/TP03_UARTinterface_data.c
+++ TP03_UARTinterface_data/TP03_UARTinterface_data.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "hardware/uart.h"
+#include "hardware/irq.h"
 
+// Definições de pinos
+#define UART0_TX_PIN 0
+#define UART0_RX_PIN 1
+#define UART1_TX_PIN 4
+#define UART1_RX_PIN 5
 
-
-int main()
-{
-    stdio_init_all();
-
-    while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
-    }
-}
