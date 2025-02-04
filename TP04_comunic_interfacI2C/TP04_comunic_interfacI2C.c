0b670d01f90675e00aabff218cdca1f767f20d38
carlosmandele <carlos.mandelle@gmail.com>
2025-02-04 00:13:12 -0300

    Add  declaração de função SetDateTime e I2C

--- TP04_comunic_interfacI2C/TP04_comunic_interfacI2C.c
+++ TP04_comunic_interfacI2C/TP04_comunic_interfacI2C.c
@@ -12,4 +12,22 @@
 volatile bool data_received = false;
 uint8_t data_buffer[BUFFER_SIZE];
 
+// Declaração de funções
+void setDateTime(int day, int month, int year, int hour, int minute, int second);
+void getDateTime();
+int decToBcd(int val);
+int bcdToDec(int val);
+
+// Função i2c
+void i2c_callback() {
+    printf("Callback i2c_callback chamado.\n");
+    int result = i2c_read_blocking(I2C_PORT, DS1307_I2C_ADDR, data_buffer, BUFFER_SIZE, false);
+    if (result == PICO_ERROR_GENERIC) {
+        printf("Erro ao ler do RTC!\n");
+    } else {
+        printf("Dados lidos com sucesso!\n");
+    }
+    data_received = true;
+}
+
 
